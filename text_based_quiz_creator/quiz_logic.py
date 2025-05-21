f07c522ff936d18b68134b8008d955d4d0edca37
Josh Obatay <joshobatay2005@gmail.com>
2025-05-21 17:43:22 +0800

    defined a class named 'Question' and added the neccessary constructors below it

--- text_based_quiz_creator/quiz_logic.py
+++ text_based_quiz_creator/quiz_logic.py
@@ -1,3 +1,23 @@
 # I will convert my prevoius code into a class-based structure.
 # This code will be used to create a quiz and read it.
 
+import json
+from pathlib import Path
+import os
+
+class Question:
+    def __init__(self, question, choices, correct_answer, explanation):
+        self.question = question
+        self.choices = choices
+        self.correct_answer = correct_answer
+        self.explanation = explanation
+
+    def convert_questions_to_dictionary(self):
+        return {
+            "Question": self.question,
+            "Choices": self.choices,
+            "Correct Answer": self.correct_answer,
+            "Explanation": self.explanation
+        }
+        
+        
