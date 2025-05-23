f1b3ca8379ac2d7e0eddd55455e43e5e84bdd539
Josh Obatay <joshobatay2005@gmail.com>
2025-05-23 15:59:54 +0800

    done copy and pasting the codes that are related only for the user interface, also double checked if there are errors especially in variable names

--- text_based_quiz_reader/user_interface.py
+++ text_based_quiz_reader/user_interface.py
@@ -1,1 +1,30 @@
 # Will copy and paste the code from the previous assignment here and make them into OOP structure
+
+import os 
+import sys
+import time
+from colorama import Fore, init
+
+init(autoreset=True) # Initialize this code for colorama to reset the color after each print statement
+
+def clear_screen():
+    os.system('cls' if os.name == 'nt' else 'clear')
+    
+def return_to_main_menu():
+    input("Press any key to return to the main menu...")   
+    clear_screen()
+    
+def loading_screen():
+    clear_screen()
+    print(Fore.CYAN + "Loading...")
+    bar_length = 30
+    for i in range(bar_length + 1):
+        time.sleep(0.1)
+        bar = "=" * (i + 1)
+        progress = int(((i + 1) / bar_length) * 100)
+        sys.stdout.write(f"\r[{bar:<{bar_length}}] {progress}%")
+        sys.stdout.flush()
+    time.sleep(0.5)
+    print("\n" + Fore.GREEN + "Ready!\n")
+    time.sleep(1)
+    clear_screen()
